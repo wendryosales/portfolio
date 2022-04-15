b01d0e3682e124911cd257f91fdcc8f0363522e4
Wendryo S <wendryo.sales@gmail.com>
2022-04-15 16:04:47 -0300

    Menu hamburguer Done

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,31 +1,40 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import style from './Header.module.css';
 
-class Header extends Component {
-  render(){
-    return (
-      <header  className={style.container}>
-        <a 
-          href='https://www.linkedin.com/in/wendryosales/'
-          className={style.logo}
-        > 
-        WS
-        </a>
-        <nav className={style.nav}>
-          <button className={style.mobile_toggle}>
-            menu
-          </button>
-          <ul className={style.menu}>
-            <li> <Link href='/' >Home</Link> </li>
-            <li> <Link href='/about' >Sobre</Link> </li>
-            <li> <Link href='/portfolio' >Portfólio</Link> </li>
-            <li> <Link href='/contact' >Contato</Link> </li>
-          </ul>
-        </nav>
-      </header>
-    )
-  }
+function Header () {
+
+  const [ menuMobile, setMenuMobile ] = useState(false);
+
+  const toggleMenu = () => setMenuMobile(!menuMobile);
+
+  return (
+    <header  className={style.container}>
+      <a 
+        href='https://www.linkedin.com/in/wendryosales/'
+        className={style.logo}
+      > 
+      WS
+      </a>
+      <nav
+        className={menuMobile ?  `${style.nav} ${style.active}` : style.nav}
+      >
+        <button 
+          className={style.mobile_toggle} 
+          onClick={toggleMenu}
+        >
+          menu
+          <span className={style.hamburguer} />
+        </button>
+        <ul className={style.menu}>
+          <li> <Link href='/' >Home</Link> </li>
+          <li> <Link href='/about' >Sobre</Link> </li>
+          <li> <Link href='/portfolio' >Portfólio</Link> </li>
+          <li> <Link href='/contact' >Contato</Link> </li>
+        </ul>
+      </nav>
+    </header>
+  )
 }
 
 export default Header;
